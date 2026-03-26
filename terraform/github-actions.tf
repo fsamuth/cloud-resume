a153c628b0d840967e487bbd21a1fdb661dda2e0
Fidèle SAMUTH <fidele@samuth.com>
2026-03-26 23:15:40 +0100

    refactor(iam): simplify inline policy, delegate reads to ReadOnlyAccess

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -34,110 +34,44 @@ resource "aws_iam_role_policy" "github_actions" {
 
 }
 
+resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
 data "aws_iam_policy_document" "github_actions_policy" {
   statement {
-    sid    = "S3"
+    sid    = "S3Write"
     effect = "Allow"
     actions = [
       "s3:PutObject",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:ListBucket",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketAcl",
-      "s3:GetBucketCORS",
-      "s3:GetBucketWebsite",
-      "s3:GetBucketVersioning",
-      "s3:GetAccelerateConfiguration",
-      "s3:GetBucketRequestPayment",
-      "s3:GetBucketLogging",
-      "s3:GetLifecycleConfiguration",
-      "s3:GetReplicationConfiguration"
+      "s3:DeleteObject"
     ]
     resources = [
-      aws_s3_bucket.resume.arn,
-      aws_s3_bucket.tfstate.arn,
-      "${aws_s3_bucket.resume.arn}/*",
-      "${aws_s3_bucket.tfstate.arn}/*"
+      "${aws_s3_bucket.resume.arn}/*"
     ]
   }
 
   statement {
-    sid    = "DynamoDB"
+    sid    = "DynamoDBLock"
     effect = "Allow"
     actions = [
       "dynamodb:PutItem",
-      "dynamodb:GetItem",
-      "dynamodb:DeleteItem",
-      "dynamodb:DescribeTable",
-      "dynamodb:DescribeContinuousBackups",
-      "dynamodb:DescribeTimeToLive",
-      "dynamodb:ListTagsOfResource"
+      "dynamodb:DeleteItem"
     ]
     resources = [
       aws_dynamodb_table.tfstate_lock.arn
     ]
-
   }
 
   statement {
-    sid    = "CloudFrontDistribution"
+    sid    = "CloudFrontInvalidation"
     effect = "Allow"
     actions = [
-      "cloudfront:CreateInvalidation",
-      "cloudfront:GetInvalidation"
+      "cloudfront:CreateInvalidation"
     ]
     resources = [
       aws_cloudfront_distribution.resume.arn
-    ]
-  }
-
-  statement {
-    sid    = "CloudFrontOAC"
-    effect = "Allow"
-    actions = [
-      "cloudfront:GetOriginAccessControl"
-    ]
-    resources = [
-      aws_cloudfront_origin_access_control.resume.arn
-    ]
-  }
-
-  statement {
-    sid    = "ACM"
-    effect = "Allow"
-    actions = [
-      "acm:GetCertificate",
-      "acm:ListTagsForCertificate",
-      "acm:DescribeCertificate"
-    ]
-    resources = [
-      aws_acm_certificate.cert.arn
-    ]
-  }
-
-  statement {
-    sid    = "IAMOpenIDConnect"
-    effect = "Allow"
-    actions = [
-      "iam:GetOpenIDConnectProvider",
-    ]
-    resources = [
-      aws_iam_openid_connect_provider.github_actions.arn
-    ]
-  }
-
-  statement {
-    sid    = "IAMRole"
-    effect = "Allow"
-    actions = [
-      "iam:GetRole",
-      "iam:ListRolePolicies",
-      "iam:GetRolePolicy",
-      "iam:ListAttachedRolePolicies"
-    ]
-    resources = [
-      aws_iam_role.github_actions.arn
     ]
   }
 }

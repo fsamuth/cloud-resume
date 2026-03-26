69e4cf0ea7888950de60a4d824dcb24eca3a14de
Fidèle SAMUTH <fidele@samuth.com>
2026-03-27 00:15:45 +0100

    fix(ci): create new certificate before destroying the old one

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,4 +2,8 @@ resource "aws_acm_certificate" "cert" {
   domain_name       = "fidele.samuth.com"
   validation_method = "DNS"
   provider          = aws.us_east_1
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }

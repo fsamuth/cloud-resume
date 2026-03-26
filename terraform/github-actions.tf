f6cac9597611bc0580dbbc025b4e96e158881810
Fidèle SAMUTH <fidele@samuth.com>
2026-03-26 23:09:19 +0100

    fix(ci): add missing actions

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -51,7 +51,8 @@ data "aws_iam_policy_document" "github_actions_policy" {
       "s3:GetAccelerateConfiguration",
       "s3:GetBucketRequestPayment",
       "s3:GetBucketLogging",
-      "s3:GetLifecycleConfiguration"
+      "s3:GetLifecycleConfiguration",
+      "s3:GetReplicationConfiguration"
     ]
     resources = [
       aws_s3_bucket.resume.arn,

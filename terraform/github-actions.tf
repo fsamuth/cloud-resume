fb821bff6a295fd6dc7c9317e16cddee3b87f17f
Fidèle SAMUTH <fidele@samuth.com>
2026-03-27 08:16:47 +0100

    refactor(iam): replace ReadOnlyAccess with scoped least privilege policy

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -34,10 +34,10 @@ resource "aws_iam_role_policy" "github_actions" {
 
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
-  role       = aws_iam_role.github_actions.name
-  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
+# resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
+#   role       = aws_iam_role.github_actions.name
+#   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+# }
 
 data "aws_iam_policy_document" "github_actions_policy" {
   statement {
@@ -84,6 +84,86 @@ data "aws_iam_policy_document" "github_actions_policy" {
       "acm:DeleteCertificate"
     ]
     resources = ["*"]
+  }
+
+  statement {
+    sid    = "S3Read"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:GetBucketPolicy",
+      "s3:GetBucketAcl",
+      "s3:GetBucketCORS",
+      "s3:GetBucketWebsite",
+      "s3:GetBucketVersioning",
+      "s3:GetBucketLogging",
+      "s3:GetBucketRequestPayment",
+      "s3:GetAccelerateConfiguration",
+      "s3:GetLifecycleConfiguration",
+      "s3:GetReplicationConfiguration",
+      "s3:GetEncryptionConfiguration",
+      "s3:GetBucketObjectLockConfiguration",
+      "s3:GetBucketPublicAccessBlock"
+    ]
+    resources = [
+      aws_s3_bucket.resume.arn,
+      aws_s3_bucket.tfstate.arn,
+      "${aws_s3_bucket.resume.arn}/*",
+      "${aws_s3_bucket.tfstate.arn}/*"
+    ]
+  }
+
+  statement {
+    sid    = "CloudFrontRead"
+    effect = "Allow"
+    actions = [
+      "cloudfront:GetDistribution",
+      "cloudfront:GetOriginAccessControl",
+      "cloudfront:ListTagsForResource"
+    ]
+    resources = [
+      aws_cloudfront_distribution.resume.arn,
+      aws_cloudfront_origin_access_control.resume.arn
+    ]
+  }
+
+  statement {
+    sid    = "ACMRead"
+    effect = "Allow"
+    actions = [
+      "acm:DescribeCertificate",
+      "acm:ListTagsForCertificate"
+    ]
+    resources = [aws_acm_certificate.cert.arn]
+  }
+
+  statement {
+    sid    = "IAMRead"
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:ListRolePolicies",
+      "iam:ListAttachedRolePolicies",
+      "iam:GetOpenIDConnectProvider"
+    ]
+    resources = [
+      aws_iam_role.github_actions.arn,
+      aws_iam_openid_connect_provider.github_actions.arn
+    ]
+  }
+
+  statement {
+    sid    = "DynamoDBRead"
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:DescribeContinuousBackups",
+      "dynamodb:DescribeTimeToLive",
+      "dynamodb:ListTagsOfResource"
+    ]
+    resources = [aws_dynamodb_table.tfstate_lock.arn]
   }
 
 }

07fdb5dac588fa0a1e4ec816fab12751f75b2227
Fidèle SAMUTH <fidele@samuth.com>
2026-03-29 00:58:04 +0100

    fix(kms): add explicit key policy

--- terraform/state.tf
+++ terraform/state.tf
@@ -38,6 +38,21 @@ resource "aws_kms_key" "tfstate_key" {
   description             = "KMS key for Terraform state bucket encryption"
   enable_key_rotation     = true
   deletion_window_in_days = 10
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_key" {
@@ -50,3 +65,5 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_key" {
     bucket_key_enabled = true
   }
 }
+
+data "aws_caller_identity" "current" {}

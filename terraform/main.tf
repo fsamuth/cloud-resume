84c128710d681a551a9312c988cd963277383612
Fidèle SAMUTH <fidele@samuth.com>
2026-03-28 08:02:10 +0100

    feat(cloudfront): redirect 403 and 404 errors to index.html

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,18 @@ resource "aws_cloudfront_distribution" "resume" {
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
 }
 
 resource "aws_s3_bucket_policy" "resume" {

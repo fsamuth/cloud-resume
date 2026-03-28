f6907af45480985e2cb666c3a0d1402bfe3b384f
Fidèle SAMUTH <fidele@samuth.com>
2026-03-28 20:39:42 +0100

    test(lambda): add unit tests for visitor counter

---
+++ website/tests/test_counter.py
@@ -0,0 +1,30 @@
+import json
+import os
+import boto3
+import pytest
+from moto import mock_aws
+
+os.environ["AWS_DEFAULT_REGION"] = "eu-west-3"
+os.environ["AWS_ACCESS_KEY_ID"] = "fake"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "fake"
+os.environ["TABLE_NAME"] = "test-visitor-counter"
+
+@pytest.fixture
+def dynamodb_table():
+    with mock_aws():
+        dynamodb = boto3.resource("dynamodb", region_name="eu-west-3")
+        table = dynamodb.create_table(
+            TableName="test-visitor-counter",
+            KeySchema=[{"AttributeName": "id", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "id", "AttributeType": "S"}],
+            BillingMode="PAY_PER_REQUEST"
+        )
+        yield table
+
+def test_first_visit_returns_one(dynamodb_table):
+    with mock_aws():
+        from counter import handler
+        response = handler({}, {})
+        assert response["statusCode"] == 200
+        body = json.loads(response["body"])
+        assert body["views"] == 1

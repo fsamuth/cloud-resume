376181aed2be1c2599a607428975da0f3a56c787
Fidèle SAMUTH <fidele@samuth.com>
2026-03-29 15:11:05 +0200

    feat(terraform): add missing outputs for key resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,19 @@ output "cloudfront_distribution_id" {
 output "api_gateway_url" {
   value = aws_apigatewayv2_stage.visitor_counter.invoke_url
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.resume.domain_name
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.visitor_counter.name
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.alerts.arn
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.visitor_counter.function_name
+}

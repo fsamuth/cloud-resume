0916745987c053c1a97e429fd1a95f42fff32979
Fidèle SAMUTH <fidele@samuth.com>
2026-04-08 22:43:08 +0200

    fix(iam): add missing S3 privileges

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -75,7 +75,8 @@ data "aws_iam_policy_document" "github_actions_policy" {
       "s3:GetBucketPublicAccessBlock",
       "s3:GetBucketTagging",
       "s3:GetObjectTagging",
-      "s3:GetBucketOwnershipControls"
+      "s3:GetBucketOwnershipControls",
+      "s3:ListBucketVersions"
     ]
     resources = [
       aws_s3_bucket.resume.arn,

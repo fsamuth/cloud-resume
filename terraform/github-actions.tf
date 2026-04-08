f0ccef3262a6fde3a7e5b5cab3d65b322df2f00a
Fidèle SAMUTH <fidele@samuth.com>
2026-04-08 23:26:46 +0200

    fix(iam): add production environment to OIDC trust policy

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -17,7 +17,8 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
       values = [
         "repo:fsamuth/cloud-resume:ref:refs/heads/main",
-        "repo:fsamuth/cloud-resume:pull_request"
+        "repo:fsamuth/cloud-resume:pull_request",
+        "repo:fsamuth/cloud-resume:environment:production"
       ]
     }
   }
